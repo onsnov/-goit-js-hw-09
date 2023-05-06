750890758b68b918a7d8e54427278b11b712b0d5
onsnov <114084570+onsnov@users.noreply.github.com>
2023-05-06 19:25:22 +0000

    Deploying to gh-pages from @ onsnov/goit-js-hw-09@4c4f8eb9c660ec6aab8b8b1e3cb43bd107e049ee 🚀

---
+++ 01-color-switcher.0a64fd82.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("body"),t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");t.addEventListener("click",(function(){a=setInterval((()=>e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`),1e3),t.disabled=!0,d.disabled=!1})),d.addEventListener("click",(function(){clearInterval(a),t.disabled=!1,d.disabled=!0}));let a=null;
+//# sourceMappingURL=01-color-switcher.0a64fd82.js.map
