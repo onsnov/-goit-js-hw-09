4c4f8eb9c660ec6aab8b8b1e3cb43bd107e049ee
onsnov <114084570+onsnov@users.noreply.github.com>
2023-05-06 22:23:28 +0300

    додає 1-є зд

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,29 @@
+const body = document.querySelector('body');
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+
+
+btnStart.addEventListener('click', startChangeBgc);
+btnStop.addEventListener('click', stopChangeBgc);
+
+
+let intervalId = null;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function startChangeBgc()
+{ 
+  intervalId = setInterval(() => body.style.backgroundColor = getRandomHexColor(), 1000);
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+};
+
+function stopChangeBgc() { 
+  clearInterval(intervalId);
+  btnStart.disabled = false;
+  btnStop.disabled = true;
 }
